1a4e6edf8d0a015b5e690c15ec13b9a1d4b46ef9
vaidehee patel <pvaidehee@gmail.com>
2020-08-12 03:01:09 +0530

    all done

--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Comments from '../Comments/Comments';
 import LikeSection from './LikeSection';
 import PostHeader from './PostHeader';
@@ -22,8 +22,10 @@ const Post = props => {
       </div>
       {/* Is LikeSection getting all the props it needs to work correctly? */}
       <LikeSection likePost={() => likePost(post.id)} />
+      <LikeSection likePost = {() => likePost(post.id)} 
+      numberOfLikes = {post.likes}/>
       {/* Comments also wants its props! */}
-      <Comments />
+      <Comments  comments = {post.comments}/>
     </div>
   );
 };
